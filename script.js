fcbfb8ed36af9a4f637f268813cdd80ec8ed3467
Remy Phan <phan.remy@gmail.com>
2019-12-23 00:45:49 +0100

    gif and animate

--- script.js
+++ script.js
@@ -9,10 +9,21 @@ document.addEventListener('scroll', (event) => {
   { passive: true }
 );
 
-const discover = document.querySelector(".discoverbtn");
-discover.addEventListener('click', (event) => {
-  // event.preventDefault;
-  // // console.log(event);
-  // const hash = this.hash;
-  // console.log(hash);
+const discovers = document.querySelectorAll(".discoverbtn");
+discovers.forEach((discover) => {
+  discover.addEventListener('click', (event) => {
+    const animfadins = document.querySelectorAll(".animfadin");
+    animfadins.forEach((animfadin) => {
+      animfadin.classList.add('animated');
+      animfadin.classList.add('fadeInUp');
+      setTimeout(function(){
+        animfadin.classList.remove('animated');
+        animfadin.classList.remove('fadeInUp');
+      },3000);
+    });
+    // event.preventDefault;
+    // // console.log(event);
+    // const hash = this.hash;
+    // console.log(hash);
+  });
 });
